343bb326fc778dfeda1827ca447b8a87c9804489
Gavril <gari@abv.bg>
2023-02-10 11:18:56 +0200

    Calculator done

--- script.js
+++ script.js
@@ -1,6 +1,67 @@
-let a = Number(document.getElementById("a").value);
-let b = Number(document.getElementById("b").value);
+const inputA = document.getElementById("inputA");
+const inputB = document.getElementById("inputB");
+const result = document.getElementById("result");
 
-if(document.getElementById("plus").onclick){
-	console.log(document.getElementById("plus"));
+const btnSum = document.getElementById("sum");
+const btnDif = document.getElementById("dif");
+const btnMul = document.getElementById("mul");
+const btnDiv = document.getElementById("div");
+const btnRoot = document.getElementById("root");
+const btnPow = document.getElementById("pow");
+const btnSin = document.getElementById("sin");
+const btnCos = document.getElementById("cos");
+const btnTg = document.getElementById("tg");
+
+btnSum.onclick = function() {
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	result.innerText = a + b;
+}
+
+btnDif.onclick = function() {
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	result.innerText = a - b;
+}
+
+btnMul.onclick = function() {
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	result.innerText = a * b;
+}
+
+btnDiv.onclick = function() {
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	result.innerText = a / b;
+}
+
+btnPow.onclick = function() {
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	result.innerText = a ** b;
+}
+
+btnRoot.onclick = function() {
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	result.innerText = a ** (1 / b);
+}
+
+btnSin.onclick = function() {
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	result.innerText = Math.sin(a / b * Math.PI);
+}
+
+btnCos.onclick = function() {
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	result.innerText = Math.cos(a / b * Math.PI);
+}
+
+btnTg.onclick = function() {
+	a = Number(inputA.value);
+	b = Number(inputB.value);
+	result.innerText = Math.tan(a / b * Math.PI);
 }

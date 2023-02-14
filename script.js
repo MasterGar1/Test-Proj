8a16e94bdb9ff925b6c23b55a27096cacc61af20
Gavril <gari@abv.bg>
2023-02-14 15:20:19 +0200

    Game +

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-let canvas = document.getElementById("canvas");
-let context = canvas.getContext("2d");
-canvas.width = 450;
-canvas.height = 300;
-context.strokeRect(5, 5, 440, 290);
+let cvs = document.getElementById("canvas");
+let ctx = cvs.getContext("2d");
+let sqs = 50;
+let sx = 5;
+let sy = 5;
+let hx = 4;
+let hy = 3;
+cvs.width = sqs * sx;
+cvs.height = sqs * sy;
+
+for(let i = 0; i < sx; i++){
+	for(let j = 0; j < sy; j++){
+		ctx.strokeRect(i * sqs, j * sqs, sqs, sqs);
+	}
+}
+
+let himg = new Image();
+himg.src = "hero.png";
+ctx.drawImage(himg, hx * sqs, hy * sqs, sqs, sqs);

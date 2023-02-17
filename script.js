9ccce058ef249502e8bfef9e4fd537dff7e444cd
Gavril <gari@abv.bg>
2023-02-17 11:28:36 +0200

    Hero moves

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 let cvs = document.getElementById("canvas");
 let ctx = cvs.getContext("2d");
-let sqs = 50;
-let sx = 5;
-let sy = 5;
-let hx = 4;
-let hy = 3;
-cvs.width = sqs * sx;
-cvs.height = sqs * sy;
 
-for(let i = 0; i < sx; i++){
-	for(let j = 0; j < sy; j++){
-		ctx.strokeRect(i * sqs, j * sqs, sqs, sqs);
-	}
+let map = {
+	sqSize: 50,
+	width: 10, 
+	height: 10
+};
+
+let hero = {
+	x: 4, 
+	y: 3,
+};
+
+let img = new Image();
+img.src = "hero.png";
+
+cvs.width = map.sqSize * map.width;
+cvs.height = map.sqSize * map.height;
+
+redrawCanvas();
+
+cvs.onmousedown = function(e) {
+	let clickX = e.x - cvs.offsetLeft;
+	let clickY = e.y - cvs.offsetTop;
+	let sqX = Math.floor(clickX / map.sqSize);
+	let sqY = Math.floor(clickY / map.sqSize);
+	moveTo(sqX, sqY);
 }
 
-let himg = new Image();
-himg.src = "hero.png";
-ctx.drawImage(himg, hx * sqs, hy * sqs, sqs, sqs);
+cvs.onkeypress = function(e) {
+	redrawCanvas();
+}
+
+function redrawCanvas(){
+	ctx.clearRect(0, 0, cvs.width, cvs.height);
+	for(let i = 0; i < map.width; i++){
+		for(let j = 0; j < map.height; j++){
+			ctx.strokeRect(i * map.sqSize, j * map.sqSize, map.sqSize, map.sqSize);
+		}
+	}
+	ctx.drawImage(img, hero.x * map.sqSize, hero.y * map.sqSize, map.sqSize, map.sqSize);
+}
+
+function moveTo(x, y) {
+	hero.x = x;
+	hero.y = y;
+	redrawCanvas();
+}
